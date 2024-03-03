c31e17a8ae0579edf90bf8177acc12962e5315e2
codepanther <pezechigozirim@gmail.com>
2024-03-03 18:32:47 +0100

    Add tailwind plugins and modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{html,js,erb}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
 
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/**/*.{html,js,erb}'
+  ],
+  theme: {
+    colors: {
+      ...colors,
+      primary: {
+        DEFAULT: '#ff5a5f',
+        dark: '#ff385c'
+      }
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries')
+  ]
+};
