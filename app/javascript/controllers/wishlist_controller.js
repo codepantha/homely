d254bec221c69ec320d3ceb35968c71788d604d2
codepanther <pezechigozirim@gmail.com>
2024-03-14 19:13:13 +0100

    Call api to wishlist property

--- app/javascript/controllers/wishlist_controller.js
+++ app/javascript/controllers/wishlist_controller.js
@@ -11,13 +11,39 @@ export default class extends Controller {
 
     let status = this.element.dataset.status;
     if (status === 'false') {
-      this.element.classList.remove('fill-none');
-      this.element.classList.add('fill-primary');
-      this.element.dataset.status = 'true';
+      this.addPropertyToWishlist()
+      
     } else {
       this.element.classList.remove('fill-primary');
       this.element.classList.add('fill-none');
       this.element.dataset.status = 'false';
+    }
+  }
+
+  async addPropertyToWishlist() {
+    const params = {
+      property_id: this.element.dataset.propertyId,
+      user_id: this.element.dataset.userId
+    };
+
+    try {
+      const response = await fetch('/api/wishlists', {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(params)
+      });
+
+      const data = await response.json();
+
+      if (data) {
+        this.element.classList.remove('fill-none');
+        this.element.classList.add('fill-primary');
+        this.element.dataset.status = 'true';
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 }
